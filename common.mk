cc69311bd121f579425c38158ed516552f125a94
oberdfr <alebennella@gmail.com>
2024-12-29 16:45:30 -0500

    universal2100-common: build necessary packages for camera provider

--- common.mk
+++ common.mk
@@ -370,7 +370,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0.vendor:64 \
     android.hardware.thermal@2.0.vendor:64 \
     android.hardware.thermal-service.pixel \
-    thermal_symlinks
+    thermal_symlinks \
+    libGrallocMapperCamera \
+    libutilscallstack.vendor \
+    libhidlmemory.vendor
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/thermal/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
