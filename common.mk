2091091442ff2e92dcd6cb645c82775e80093f4f
Bruno Martins <bgcngm@gmail.com>
2025-05-18 13:26:33 -0400

    powershare: Migrate to AIDL
    Change-Id: I2ae661bf0069c7f1dbed3f5be4275efb9b763352

--- common.mk
+++ common.mk
@@ -297,7 +297,7 @@ PRODUCT_PACKAGES += \
 
 # PowerShare
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.samsung
+    vendor.lineage.powershare-service.samsung
 
 # Protobuf
 PRODUCT_PACKAGES += \
